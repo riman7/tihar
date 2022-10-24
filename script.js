b95a99424c788ca7429fa716e0a0525df5983fab
riman7 <r.i.m.a.n.maharjan7@gmail.com>
2022-10-24 19:49:09 +0545

    Major changes

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
 let array = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "a", "b", "c", "d", "e", "f"];
-randomNumber = () =>{
-    return Math.floor(Math.random() * array.length);
-}
-
-getRandomTime = () =>{
-    return (Math.random() * 3);
-}
-getRandomBorderSize = () =>{
-    return Math.floor(Math.random() * 15 + 15);
-}
-getRandomBlur = () => {
-    return Math.floor(Math.random() * 4 + 1);
-}
-
 function colorChange(){
     let color = "#";
     for(let i = 1; i<=6; i++){
@@ -20,13 +6,36 @@ function colorChange(){
     }
     return color;
 }
+
+randomNumber = () =>{
+    return Math.floor(Math.random() * array.length);
+}
+getRandomTime = () =>{
+    return (Math.random() * 3 + 1);
+}
+getRandomCircleSize = () =>{
+    return Math.floor(Math.random() * 3 + 2);
+}
+
+
 start = () => {
-    for(let i=1; i<=100; i++){
-        document.querySelector(".b" + i).style.transition = "ease " + getRandomTime() + "s";
-        document.querySelector(".b" + i).style.backgroundColor = colorChange();
-        document.querySelector(".b" + i).style.border = "solid" + colorChange() + " " + getRandomBorderSize() + "px";
-        document.querySelector(".b" + i).style.filter = "blur(" + getRandomBlur() + "px)";
-        document.querySelector(".b" + i).style.boxShadow = "0 0 " + getRandomBorderSize() + "px"+ colorChange() ;
-    }
+    circle = document.querySelectorAll('.circle');
+circle.forEach(myFunction);
+function myFunction(item, index){
+    let  box = document.querySelector(`.b${index + 1}`);
+    let wh = getRandomCircleSize();
+    let col = colorChange();
+    let col2 = colorChange();
+    item.style.width = `min(${wh}vh, ${wh }vw)`;
+    item.style.height = `min(${wh}vh, ${wh }vw)`;
+    item.style.backgroundColor = col;
+    item.style.boxShadow = `0 0 min(${wh - 1}vh, ${wh - 1}vw)  ${col}`;
+
+    box.style.border = "solid" + col + " 4px";
+    box.style.transition = "ease all " + getRandomTime() + "s";
+    box.style.backgroundColor = col2; 
+    box.style.boxShadow = `0 0 20px ${col}`;
+    box.style.filter = `blur(${.1 * wh}px) saturate(125%)`
+}
 }
 setInterval(start, 1500);
